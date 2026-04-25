30a15cf4d8efd08d3e50b6924335cce2af248b77
fressysharon-h <fressyshutabarat@gmail.com>
2026-04-26 00:32:10 +0700

    Implement Hewan class with MakhlukHidup methods

---
+++ Modul 8/latihanInterface/Hewan.java
@@ -0,0 +1,17 @@
+public class Hewan implements MakhlukHidup {
+
+    @Override
+    public void makan() {
+        System.out.println("Makan pakai mulut");
+    }
+
+    @Override
+    public void berjalan() {
+        System.out.println("Jalan pakai 4 kaki");
+    }
+
+    @Override
+    public void bersuara() {
+        System.out.println("Suaranya tidak jelas");
+    }
+}
